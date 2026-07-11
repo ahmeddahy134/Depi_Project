c932c5d9ed90d7d4b5db80670aadfceb03ee96df
bavlya954 <bavlya954@gmail.com>
2026-07-11 15:49:18 +0300

    Complete Phase 3 Kubernetes

--- infrastructure/terraform/environments/dev/main.tf
+++ infrastructure/terraform/environments/dev/main.tf
@@ -25,9 +25,9 @@ module "eks" {
   vpc_id             = module.vpc.vpc_id
   subnet_ids         = module.vpc.private_subnet_ids
   node_instance_type = "t3.medium"
-  node_desired_size  = 2
+  node_desired_size  = 4
   node_min_size      = 2
-  node_max_size      = 4
+  node_max_size      = 5
 }
 
 module "ecr" {
--- infrastructure/terraform/environments/prod/providers.tf
+++ infrastructure/terraform/environments/prod/providers.tf
@@ -1,19 +1,19 @@
-terraform {{
+terraform {
   required_version = ">= 1.7.0"
-  required_providers {{
-    aws = {{
+  required_providers {
+    aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-    }}
-  }}
-}}
+    }
+  }
+}
 
-provider "aws" {{
+provider "aws" {
   region = var.aws_region
-}}
+}
 
 # Separate provider alias required for ACM certs used by CloudFront (must be us-east-1)
-provider "aws" {{
+provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
-}}
+}
